52c6f16dc95fb0d5b7270bc2575524d3ce5e9af9
SaiShashank-zelar <shashank@zelarsoft.com>
2021-09-21 11:57:13 +0530

    update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,6 @@ const  createProxyMiddleware  = require('http-proxy-middleware');
 const url = process.env.CONTAINER_NAME
 module.exports = function(app) {
   app.use(
-    '/customers',
     createProxyMiddleware({
       target: url,
       changeOrigin: true,
