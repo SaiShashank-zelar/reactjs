6e44d0389af4997541a46316f1531a43924da7a3
SaiShashank-zelar <shashank@zelarsoft.com>
2021-09-21 20:03:49 +0530

    update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -10,9 +10,19 @@ const url = process.env.CONTAINER_NAME;
 //  );
 //};
 
-const proxy = require('http-proxy-middleware');
-
-module.exports = function(app) {
-  app.use(proxy( { target: url }))
-
-}
+//const proxy = require('http-proxy-middleware');
+//
+//module.exports = function(app) {
+//  app.use(proxy('/', { target: url }))
+//
+//}
+const PROXY_CONFIG = [
+   {
+    context: ['/customers'],
+    target: url,
+    secure: false,
+    logLevel: 'debug',
+    pathRewrite: {'^/customers' : ''}
+  }
+];
+module.exports = PROXY_CONFIG;
